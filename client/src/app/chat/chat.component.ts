a2adc6dec0a85b50a05a919e20ad0a2f17ef7319
glozano <gazzon@gmail.com>
2018-03-05 16:35:19 -0300

    Nientiendo / Blabla.me v1

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef } from '@angular/core';
+import { Component, OnInit, OnChanges, ViewChildren, ViewChild, AfterViewInit, QueryList, ElementRef } from '@angular/core';
 import { MatDialog, MatDialogRef, MatList, MatListItem } from '@angular/material';
-
+import {trigger, state, style, animate, transition} from '@angular/animations';
 import { Action } from './shared/model/action';
 import { Event } from './shared/model/event';
 import { Message } from './shared/model/message';
@@ -8,29 +8,47 @@ import { User } from './shared/model/user';
 import { SocketService } from './shared/services/socket.service';
 import { DialogUserComponent } from './dialog-user/dialog-user.component';
 import { DialogUserType } from './dialog-user/dialog-user-type';
+import axios from 'restyped-axios'
+import { GiphyAPI } from 'restyped-giphy-api'
 
-
+const client = axios.create<GiphyAPI>({baseURL: 'http://api.giphy.com/v1'});
 const AVATAR_URL = 'https://api.adorable.io/avatars/285';
+const spinner = 'assets/spinner.gif';
 
 @Component({
   selector: 'tcc-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  animations: [
+      trigger('btnState', [
+          state('inactive', style({
+              opacity: 0
+          })),
+          state('active',   style({
+              opacity: 1
+          })),
+          transition('inactive => active', animate('500ms ease-in')),
+          transition('active => inactive', animate('80ms ease-out'))
+      ])
+  ]
 })
 export class ChatComponent implements OnInit, AfterViewInit {
   action = Action;
   user: User;
   messages: Message[] = [];
   messageContent: string;
+  btnState = 'inactive';
   ioConnection: any;
   dialogRef: MatDialogRef<DialogUserComponent> | null;
   defaultDialogUserParams: any = {
     disableClose: true,
     data: {
-      title: 'Welcome',
+      title: 'Bienvenido, ',
       dialogType: DialogUserType.NEW
     }
   };
+  currentGifs: any[] = [];
+  chatHeight: number;
 
   // getting a reference to the overall list, which is the parent container of the list items
   @ViewChild(MatList, { read: ElementRef }) matList: ElementRef;
@@ -38,15 +56,29 @@ export class ChatComponent implements OnInit, AfterViewInit {
   // getting a reference to the items/messages within the list
   @ViewChildren(MatListItem, { read: ElementRef }) matListItems: QueryList<MatListItem>;
 
+  @ViewChild('chatContainer') chatContainer: ElementRef;
+  @ViewChild('chatFooter') chatFooter: ElementRef;
+
   constructor(private socketService: SocketService,
     public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.initModel();
+    // this.initGifs();
     // Using timeout due to https://github.com/angular/angular/issues/14748
     setTimeout(() => {
       this.openUserPopup(this.defaultDialogUserParams);
     }, 0);
+
+    setTimeout(() => {
+        this.showButton();
+    }, 1000);
+
+  }
+
+  errorHandler(event) {
+      console.debug(event);
+      event.target.src = "assets/spinner.gif";
   }
 
   ngAfterViewInit(): void {
@@ -64,6 +96,18 @@ export class ChatComponent implements OnInit, AfterViewInit {
     } catch (err) {
     }
   }
+
+  private hideButton():void {
+      this.btnState = 'inactive';
+  }
+
+    private showButton():void {
+        this.btnState = 'active';
+    }
+
+  // private initGifs(): void {
+      // this.giphyTrending();
+  // }
 
   private initModel(): void {
     const randomId = this.getRandomId();
@@ -101,11 +145,19 @@ export class ChatComponent implements OnInit, AfterViewInit {
     this.openUserPopup({
       data: {
         username: this.user.name,
-        title: 'Edit Details',
+        title: '¿Quieres cambiar?',
         dialogType: DialogUserType.EDIT
       }
     });
   }
+
+  private adjustHeights(): void {
+      if(this.chatFooter.nativeElement.offsetHeight < 100){
+        this.chatHeight = this.chatFooter.nativeElement.offsetTop - 100;
+      }else{
+        this.chatHeight = this.chatFooter.nativeElement.offsetTop;
+      }
+  };
 
   private openUserPopup(params): void {
     this.dialogRef = this.dialog.open(DialogUserComponent, params);
@@ -124,7 +176,7 @@ export class ChatComponent implements OnInit, AfterViewInit {
     });
   }
 
-  public sendMessage(message: string): void {
+  public sendMessage(message: any): void {
     if (!message) {
       return;
     }
@@ -134,6 +186,46 @@ export class ChatComponent implements OnInit, AfterViewInit {
       content: message
     });
     this.messageContent = null;
+    this.currentGifs = [];
+    this.showButton();
+    setTimeout((): void => {this.adjustHeights()}, 500);
+  }
+
+    // public giphyTrending(): any {
+    //     client.request( {
+    //         url: '/gifs/trending',
+    //         params: {
+    //             api_key: '9dHK7nkPwZxfNdkiLFUt9MidUP7jxidQ',
+    //             limit: 8
+    //         }
+    //     }).then((res) => {
+    //         for (const gif of res.data.data){
+    //             this.currentGifs.push(gif);
+    //         };
+    //         setTimeout((): void => {this.adjustHeights()}, 500);
+    //     })
+    // }
+
+  public searchGiphy(message: string): void {
+      client.request({
+          url: '/gifs/search',
+          params: {
+              api_key: '9dHK7nkPwZxfNdkiLFUt9MidUP7jxidQ',
+              q: message,
+              limit: 15
+          }
+      }).then((res) => {
+        this.currentGifs = [];
+        for (const gif of res.data.data){
+            this.currentGifs.push(gif);
+        };
+        setTimeout((): void => {this.adjustHeights()}, 500);
+      })
+  }
+
+  public cleanGifs(): void {
+      this.currentGifs = [];
+      this.showButton();
   }
 
   public sendNotification(params: any, action: Action): void {
