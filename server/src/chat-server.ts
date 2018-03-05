370d76b4347c64d84e9a965a10cdb1c554799959
glozano <gazzon@gmail.com>
2018-03-05 17:02:36 -0300

    Changed port to 4080

--- server/src/chat-server.ts
+++ server/src/chat-server.ts
@@ -5,7 +5,7 @@ import * as socketIo from 'socket.io';
 import { Message } from './model';
 
 export class ChatServer {
-    public static readonly PORT:number = 8080;
+    public static readonly PORT:number = 4080;
     private app: express.Application;
     private server: Server;
     private io: SocketIO.Server;
